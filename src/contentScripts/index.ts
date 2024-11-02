55455bb29526f6058e397412ae1a7f2c5ba586b4
Ivan Melentyev <ivan.saxalin@mail.ru>
2024-11-03 00:34:27 +0300

    fix: подключение стилей не работает при переходе между страницами

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -4,28 +4,32 @@ import { createApp } from 'vue'
 import App from './views/App.vue'
 import { setupApp } from '~/logic/common-setup'
 
-// Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
+const styleEl = document.createElement('link')
+styleEl.setAttribute('rel', 'stylesheet')
+styleEl.setAttribute('href', browser.runtime.getURL('dist/contentScripts/style.css'))
+const container = document.createElement('div')
+container.id = __NAME__
+const root = document.createElement('div')
+
+function addAll() {
+  document.body.appendChild(styleEl)
+  document.body.appendChild(root)
+  document.body.appendChild(container)
+}
+
+// Firefox `browser.tabs.executeScript()` требует, чтобы скрипты возвращали примитивное значение
 (() => {
   console.info('[vitesse-webext] Hello world from content script')
 
-  // communication example: send previous tab title from background page
+  // пример коммуникации: отправьте заголовок предыдущей вкладки с фоновой страницы
   onMessage('tab-prev', ({ data }) => {
     console.log(`[vitesse-webext] Navigate from page "${data.title}"`)
   })
 
-  // mount component to context window
-  const container = document.createElement('div')
-  container.id = __NAME__
-  const root = document.createElement('div')
-  const styleEl = document.createElement('link')
-  // const shadowDOM = container.attachShadow?.({ mode: __DEV__ ? 'open' : 'closed' }) || container
-  styleEl.setAttribute('rel', 'stylesheet')
-  styleEl.setAttribute('href', browser.runtime.getURL('dist/contentScripts/style.css'))
-  // shadowDOM.appendChild(styleEl)
-  // shadowDOM.appendChild(root)
-  document.body.appendChild(styleEl)
-  document.body.appendChild(root)
-  document.body.appendChild(container)
+  window.addEventListener('popstate', addAll)
+  document.addEventListener('turbolinks:load', addAll)
+  addAll()
+
   const app = createApp(App)
   setupApp(app)
   app.mount(root)
